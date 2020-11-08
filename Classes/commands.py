909f71016be7c9a62bfa86b32436fe5e8040ab26
hrolfurgylfa <hrolfurgylfa@protonmail.com>
2020-11-08 17:47:30 +0000

    Allowed Dev Team members to access some needed commands.

--- Classes/commands.py
+++ Classes/commands.py
@@ -800,8 +800,8 @@ class VRChatAccoutLink(commands.Cog):
             )
 
     @commands.command()
-    @checks.is_white_shirt()
-    @checks.is_admin_bot_channel()
+    @checks.is_team_bot_channel()
+    @commands.check_any(checks.is_white_shirt(), checks.is_dev_team())
     async def lvn(self, ctx):
         """
         This command is used to get the VRChat names of the people that are LPD Officers.
@@ -959,8 +959,8 @@ class Other(commands.Cog):
 
         return string
 
-    @checks.is_admin_bot_channel()
-    @checks.is_white_shirt()
+    @checks.is_team_bot_channel()
+    @commands.check_any(checks.is_white_shirt(), checks.is_dev_team())
     @commands.command()
     async def rtv(self, ctx, role_name):
         """
