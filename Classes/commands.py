06eeb60f6ab5c57debe6c1cd7001783874ac36e0
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2020-11-29 14:30:24 -0600

    Update commands.py

--- Classes/commands.py
+++ Classes/commands.py
@@ -729,7 +729,9 @@ class Inactivity(commands.Cog):
         role_ids = role_id_index(bot.settings)
         officers_to_check = []
         guild = self.bot.officer_manager.guild
-
+        
+        
+        '''
         for member in guild.members:
             for role in member.roles:
                 if (
@@ -746,8 +748,15 @@ class Inactivity(commands.Cog):
                     # If they've gone past the inactivity limit, add them to the list
                     if last_activity < oldest_valid:
                         inactive_officers.append(member)
+        '''
         
-        
+        for officer in OfficerManager.all_officers:
+            if officer.id not in loa_officer_ids:
+                last_activity = await officer.get_last_activity(ctx.bot.officer_manager.all_monitored_channels)
+                last_activity = last_activity["time"]
+                if last_activity < oldest_valid:
+                    inactive_officers.append(officer)
+                    
         role = self.bot.officer_manager.guild.get_role(
             self.bot.settings["inactive_role"]
         )
