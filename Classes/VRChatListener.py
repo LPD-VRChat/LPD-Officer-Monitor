572e4df8a6565ce5e67a4c5a54ed0915ebc87aaf
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2020-12-12 16:43:48 -0600

    Update VRChatListener.py

--- Classes/VRChatListener.py
+++ Classes/VRChatListener.py
@@ -7,8 +7,8 @@ loop = asyncio.get_event_loop()
 client = vrcpy.Client(loop=loop)
 
 def printd(string):
-    timestamp = (str(datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")), 'white')
-    string = (colored(timestamp, 'white') + ' - ' + string)
+    timestamp = (str(datetime.now().strftime("%d-%b-%Y (%H:%M:%S)")))
+    string = colored(timestamp, 'white') + ' - ' + string
     print(string)
 
 
