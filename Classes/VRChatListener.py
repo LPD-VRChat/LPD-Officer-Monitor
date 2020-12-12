ef447ed259aafe360eeb835960068d475cedcde2
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2020-12-12 16:37:19 -0600

    Update VRChatListener.py

--- Classes/VRChatListener.py
+++ Classes/VRChatListener.py
@@ -1,8 +1,14 @@
 import vrcpy
 import asyncio
+from termcolor import colored
+from datetime import datetime
 
 loop = asyncio.get_event_loop()
 client = vrcpy.Client(loop=loop)
+
+def printd(string):
+    string = (str(datetime.now()) + ' - ' + string)
+    print(string)
 
 
 async def main(username, password):
@@ -23,52 +29,53 @@ async def start(username, password):
 
 @client.event
 async def on_friend_location(friend_b, friend_a):
-    #print(friend_a.__dict__)
-    print("{} is now in {}#{}.".format(friend_a.display_name, friend_a.location, friend_a.instance_id))
+    #printd(friend_a.__dict__)
+    printd("{} is now in {}#{}.".format(colored(friend_a.display_name, 'green'),
+                                       colored(friend_a.location, 'yellow'), friend_a.instance_id))
     #                                "a private world" if friend_a.location is None else friend_a.location))
 
 
 #@client.event
 #async def on_friend_offline(friend_a):
-#    print("{} went offline.".format(friend_a.display_name))
+#    printd("{} went offline.".format(colored(friend_a.display_name, 'green')))
 
 
 @client.event
 async def on_friend_active(friend_a):
-    print("{} is now {}.".format(friend_a.display_name, friend_a.state))
+    printd("{} is now {}.".format(colored(friend_a.display_name, 'green'), friend_a.state))
 
 
 @client.event
 async def on_friend_online(friend_a):
-    print("{} is now online.".format(friend_a.display_name))
+    printd("{} is now online.".format(colored(friend_a.display_name, 'green')))
 
 
 @client.event
 async def on_friend_add(friend_b, friend_a):
-    print("{} is now your friend.".format(friend_a.display_name))
+    printd("{} is now your friend.".format(colored(friend_a.display_name, 'green')))
 
 
 @client.event
 async def on_friend_delete(friend_b, friend_a):
-    print("{} is no longer your friend.".format(friend_a.display_name))
+    printd("{} is no longer your friend.".format(colored(friend_a.display_name, 'green')))
 
 
 #@client.event
 #async def on_friend_update(friend_b, friend_a):
-#    print("{} has updated their profile/account.".format(friend_a.display_name))
+#    printd("{} has updated their profile/account.".format(colored(friend_a.display_name, 'green')))
 
 
 #@client.event
 #async def on_notification(notification):
-#    print("Got a {} notification from {}.".format(
+#    printd("Got a {} notification from {}.".format(
 #        notification.type, notification.senderUsername))
 
 
 @client.event
 async def on_connect():
-    print("Connected to wss pipeline.")
+    printd("Connected to wss pipeline.")
 
 
 @client.event
 async def on_disconnect():
-    print("Disconnected from wss pipeline.")
+    printd("Disconnected from wss pipeline.")
