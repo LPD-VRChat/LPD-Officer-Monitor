7141f35b1f9e62241aca18667f34959a45fdb1bb
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2020-12-13 05:43:13 -0600

    Update VRChatListener.py

--- Classes/VRChatListener.py
+++ Classes/VRChatListener.py
@@ -31,12 +31,11 @@ async def start(username, password):
 @client.event
 async def on_friend_location(friend_b, friend_a):
     world = await client.fetch_instance_via_id(friend_a.world_id, friend_a.instance_id)
-    print(world[name])
+    print(world)
     #location = world.name
     
-    #printd(friend_a.__dict__)  # Used this for diagnosis
-    printd("{} is now in {}#{}.".format(colored(friend_a.display_name, 'green'),
-                                       colored(location, 'yellow'), friend_a.instance_id))
+    #printd("{} is now in {}#{}.".format(colored(friend_a.display_name, 'green'),
+    #                                   colored(location, 'yellow'), friend_a.instance_id))
     #                                "a private world" if friend_a.location is None else friend_a.location))
 
 
