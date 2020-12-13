aae89fe42dcf3f137d812916ff5087952e6818f0
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2020-12-13 07:23:00 -0600

    Update VRChatListener.py

--- Classes/VRChatListener.py
+++ Classes/VRChatListener.py
@@ -34,7 +34,7 @@ async def stop():
 async def on_friend_location(friend_b, friend_a):
     instance = await client.fetch_instance_via_id(friend_a.world_id, friend_a.instance_id)
     world = instance.get_world()
-    print(world)
+    print(world['name'])
     
     #print(friend_a.world_id)
     #print(world.__dict__)
