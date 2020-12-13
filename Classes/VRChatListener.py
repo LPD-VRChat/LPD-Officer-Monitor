3b69e585688cafce8d60646156ce6c0cf82bba4f
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2020-12-13 05:51:16 -0600

    Update VRChatListener.py

--- Classes/VRChatListener.py
+++ Classes/VRChatListener.py
@@ -33,7 +33,7 @@ async def on_friend_location(friend_b, friend_a):
     try:
         world = await client.fetch_instance_via_id(friend_a.world_id, friend_a.instance_id)
         print(world.__dict__)
-        
+
     #location = world.name
     
     #printd("{} is now in {}#{}.".format(colored(friend_a.display_name, 'green'),
