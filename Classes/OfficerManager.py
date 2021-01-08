5946b33b1ce490b96cfe08a17641d4a0e0528e38
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2021-01-08 17:55:15 -0600

    Update OfficerManager.py

--- Classes/OfficerManager.py
+++ Classes/OfficerManager.py
@@ -67,7 +67,7 @@ class OfficerManager:
 
         # Fetch all the officers from the database
         try:
-            result = bot.sql.request("SELECT officer_id FROM Officers")
+            result = await bot.sql.request("SELECT officer_id FROM Officers")
             
         except Exception as error:
             print("ERROR failed to fetch officers from database:")
