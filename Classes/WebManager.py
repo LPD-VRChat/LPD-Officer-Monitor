2b30317b15367e07c7b539fa4a768e7b0264ae41
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2021-03-06 12:36:20 -0600

    changed port and host

--- Classes/WebManager.py
+++ Classes/WebManager.py
@@ -60,7 +60,7 @@ class WebManager:
 
         #   endpoint_path = "https://discord.com/api/oauth2/authorize?client_id=764230749992779806&redirect_uri=http%3A%2F%2Fdevbox.lolipd.com%2Fcallback&response_type=code&scope=identify"
         loop = asyncio.get_event_loop()
-        app.run(loop=loop)
+        app.run(loop=loop, host=host, port=port)
 
     @app.route("/callback/")
     async def _callback():
