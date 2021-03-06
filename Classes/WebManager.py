593904f159168643d3343fadf42f03631ed53b75
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2021-03-06 14:25:33 -0600

    added user validation to pages

--- Classes/WebManager.py
+++ Classes/WebManager.py
@@ -7,6 +7,37 @@ from quart_discord import DiscordOAuth2Session, requires_authorization, Unauthor
 
 app = Quart("LPD Officer Monitor")
 
+HTML_HEAD = """<!DOCTYPE html>
+            <HTML lang="en" xmlns="http://www.w3.org/1999/xhtml">
+            <HEAD>
+                <meta charset="utf-8" />
+                <TITLE>{}</TITLE>
+                <style>
+                    a:link,a:visited {
+                    color: Blue;
+                    background-color: White;
+                    text-decoration: underline;
+                    target-new: none;
+                    }
+                    a:hover {
+                    color: Blue;
+                    background-color: Yellow;
+                    text-decoration: underline;
+                    target-new: none;
+                    }
+                </style>
+            </HEAD>
+            <BODY>
+                <div class="topnav">
+                    <a class="active" href="/">Home</a>
+                        <a href="/login">Login</a>
+                        <a href="/officers">Officers</a>
+                        <a href="/officers_only">Officers only</a>
+                        <a href="/moderartion">Moderation</a>
+                </div>"""
+
+HTML_FOOT = """</BODY></HTML>"""
+
 
 @app.route("/callback/")
 async def callback():
@@ -64,7 +95,7 @@ class WebManager:
     @app.route("/callback/")
     async def _callback():
         await discord.callback()
-        return redirect(url_for(".me"))
+        return redirect(url_for(".home"))
 
     @app.errorhandler(Unauthorized)
     async def redirect_unauthorized(e):
@@ -74,43 +105,15 @@ class WebManager:
     @requires_authorization
     async def _me():
         user = await discord.fetch_user()
-        return f"""
-        <html>
-            <head>
-                <title>{user.name}</title>
-            </head>
-            <body>
-                <img src='{user.avatar_url}' />
-            </body>
-        </html>"""
+        return f"""{HTML_HEAD.format(user.name)}
+            <img src='{user.avatar_url}' />
+            {HTML_FOOT}"""
 
     @app.route("/")
     async def home():
-        content = """<!DOCTYPE html>
-            <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-            <head>
-                <meta charset="utf-8" />
-                <title>Welcome to the Loli Police Department!</title>
-                <style>
-                    a:link,a:visited {
-                    color: Blue;
-                    background-color: White;
-                    text-decoration: underline;
-                    target-new: none;
-                    }
-                    a:hover {
-                    color: Blue;
-                    background-color: Yellow;
-                    text-decoration: underline;
-                    target-new: none;
-                    }
-                </style>
-            </head>
-            <body>
-                <a href="/officers">Table of All Officers</a><br><br>
-                <a href="/login">Login</a>
-            </body>
-            </html>"""
+        content = f"""{HTML_HEAD.format('Welcome to the LPD!')}
+            Welcome to the home page.
+            {HTML_FOOT}"""
         return content
 
     @app.route("/login/")
@@ -122,13 +125,8 @@ class WebManager:
     async def display_officers():
 
         user = await discord.fetch_user()
-        content = f"""<!DOCTYPE html>
-            <html lang="en" xmlns="http://www.w3.org/1999/xhtml">
-            <head>
-                <meta charset="utf-8" />
-                <title>List of all officers</title>
-            </head>
-            <body>Welcome {user.name} - your ID  is {user.id}<br><br>
+        content = f"""{HTML_HEAD.format('Table of Officers')}
+            Welcome {user.name} - your ID  is {user.id}<br><br>
             <table style="width:100%">
             <tr>
                 <th>Officer ID</th>
@@ -146,6 +144,42 @@ class WebManager:
                         <td>{officer.squad}</td>
                         </tr>"""
         content = f"""{content}
-                    </table></body></html>"""
+                    </table>{HTML_FOOT}"""
+
+        return content
+
+    @app.route("/officers_only")
+    @requires_authorization
+    async def _officers_page():
+        user = await discord.fetch_user()
+
+        if user.id not in bot.officer_manager.all_officer_ids:
+            content = f"""{htmL_HEAD.format('This page is restricted to LPD Officers only')}
+            Sorry, this page is restricted to Officers of the LPD only.
+            {HTML_FOOT}"""
+
+            return content
+
+        content = f"""{HTML_HEAD.format('LPD Officers only')}
+            This page is for LPD Officers only. It looks like you're an officer, so welcome!
+            {HTML_FOOT}"""
+
+        return content
+
+    @app.route("/moderation")
+    @requires_authorization
+    async def _moderation_page():
+        user = await discord.fetch_user()
+        officer = bot.officer_manager.get_officer(user.id)
+        if not officer.is_white_shirt():
+            content = f"""{HTML_HEAD.format('This page is for LPD White Shirts only.')}
+                Sorry, you're not staff.
+                {HTML_FOOT}"""
+
+            return content
+        content = f"""{HTML_HEAD.format('LPD Moderator Portal')}
+            <h1 style="color: #4485b8;">LPD Moderator portal</h1>
+            <p><span style="color: #000000;"><b>This is a test page for LPD Moderators only.</b></span></p>
+            {HTML_FOOT}"""
 
         return content
