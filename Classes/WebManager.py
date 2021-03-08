4fbcaef9163eeaec9a9836341f09d7a395e2663e
CaptainDestructo <72361870+CaptainDestructo@users.noreply.github.com>
2021-03-07 23:20:33 -0600

    Fixed an error

--- Classes/WebManager.py
+++ Classes/WebManager.py
@@ -509,7 +509,8 @@ class WebManager:
                     if last_activity < oldest_valid:
                         inactive_officers.append(officer, last_activity, has_role)
                 except:
-                    return f"""{HTML_HEAD.format('Inactivity - NO DATA')}<br><br>It looks like there isn't any patrol data... </body>{HTML_FOOT}"""
+                    pass
+                    # return f"""{HTML_HEAD.format('Inactivity - NO DATA')}<br><br>It looks like there isn't any patrol data... </body>{HTML_FOOT}"""
 
         if len(inactive_officers) == 0:
             return f"""{HTML_HEAD.format('Inactivity - NONE INACTIVE')}<br><br>It doesn't look like there are any inactive officers!</body>{HTML_FOOT}"""
