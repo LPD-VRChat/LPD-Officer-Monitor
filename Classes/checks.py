d335e0671cde05f1005cf7fad9925b8b4c9a376b
CaptainDestructo <kv4lq.dan@gmail.com>
2021-06-06 23:42:17 +0000

    Changed event_bot_channel check to team_bot_channel

--- Classes/checks.py
+++ Classes/checks.py
@@ -92,20 +92,6 @@ def is_application_channel():
     return commands.check(predicate)
 
 
-def is_event_bot_channel():
-    def predicate(ctx):
-        if (
-            ctx.message.channel.id != ctx.bot.settings["admin_bot_channel"]
-            and ctx.message.channel.id != ctx.bot.settings["event_bot_channel"]
-        ):
-            raise errors.WrongChannelError(
-                "This command only works in the event bot channel."
-            )
-        return True
-
-    return commands.check(predicate)
-
-
 def is_team_bot_channel():
     def predicate(ctx):
         admin_bot_channel = ctx.bot.officer_manager.guild.get_channel(
