7246fa6fa73a856b8ae11a916746c7268adeffb9
CaptainDestructo <kv4lq.dan@gmail.com>
2021-07-19 09:06:16 +0000

    Fixed shutdown console output

--- UILayer/DiscordCommands/Programming.py
+++ UILayer/DiscordCommands/Programming.py
@@ -28,7 +28,7 @@ class Programming(commands.Cog):
         """This command shuts the bot down cleanly"""
         await ctx.send("Shutting the bot down...")
         await clean_shutdown(
-            self.bot, ctx.channel.name, ctx.author.display_name, exit=True
+            self.bot, "#" + ctx.channel.name, ctx.author.display_name, exit=True
         )
 
     @checks.is_team_bot_channel()
@@ -38,7 +38,7 @@ class Programming(commands.Cog):
         """Restarts the bot if something is broken"""
         await ctx.send("Restarting the bot...")
         await clean_shutdown(
-            self.bot, ctx.channel.name, ctx.author.display_name, exit=False
+            self.bot, "#" + ctx.channel.name, ctx.author.display_name, exit=False
         )
 
     @checks.is_team_bot_channel()
@@ -51,15 +51,31 @@ class Programming(commands.Cog):
     @checks.is_team_bot_channel()
     @checks.is_programming_team()
     @commands.command()
-    async def reload_module(self, ctx, module_name: str):
+    async def reload(self, ctx, module_name: str):
         """Reloads a module"""
-        module = f"UILayer.DiscordCommands.{module_name}"
-        try:
-            self.bot.reload_extension(module)
-            await ctx.send(f"Successfully reloaded {module_name}")
-        except Exception as e:
-            await ctx.send(f"Failed to reload {module_name}")
-            await handle_error(self.bot, e, traceback.format_exc)
+        if module_name.lower() == "all":
+            extensions = [name for name in self.bot.extensions.keys()]
+            for m in extensions:
+                try:
+                    self.bot.reload_extension(m)
+                    await ctx.send(f"""Successfull reloaded {m.split('.')[-1]}""")
+                except Exception as e:
+                    await ctx.send(f"""Failed to reload {m.split('.')[-1]}""")
+                    await handle_error(self.bot, e, traceback.formaW_exc())
+                print(
+                    f"{ctx.author.display_name} reloaded {m.split('.')[-1]} from #{ctx.channel.name}"
+                )
+        else:
+            module = f"UILayer.DiscordCommands.{module_name}"
+            try:
+                self.bot.reload_extension(module)
+                await ctx.send(f"Successfully reloaded {module_name}")
+                print(
+                    f"{ctx.author.display_name} reloaded {module_name} from #{ctx.channel.name}"
+                )
+            except Exception as e:
+                await ctx.send(f"Failed to reload {module_name}")
+                await handle_error(self.bot, e, traceback.format_exc())
 
 
 def setup(bot):
