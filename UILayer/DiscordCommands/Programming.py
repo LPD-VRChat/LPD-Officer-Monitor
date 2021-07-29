f3aae381adf8ffc78d874dc0012fd8b1ed156934
Hrólfur <hrolfurgylfa@protonmail.com>
2021-07-29 22:20:23 +0000

    Fixed bot.bl_wrapper missing and added logging for reload

--- UILayer/DiscordCommands/Programming.py
+++ UILayer/DiscordCommands/Programming.py
@@ -5,6 +5,7 @@ import Settings
 import traceback
 import asyncio
 import argparse
+import logging
 
 # Community
 import discord
@@ -15,9 +16,13 @@ from BusinessLayer.extra_functions import handle_error
 import BusinessLayer.checks as checks
 
 
+log = logging.getLogger("lpd-officer-monitor")
+
+
 class Programming(commands.Cog):
     def __init__(self, bot):
         self.bl_wrapper = bot.bl_wrapper
+        self.bot = bot
         self.color = discord.Color.red()
 
     @checks.is_team_bot_channel()
@@ -52,33 +57,42 @@ class Programming(commands.Cog):
     @commands.command()
     async def reload(self, ctx, module_name: str):
         """Reloads a module"""
-        if module_name.lower() == "all":
-            extensions = [name for name in self.bot.extensions.keys()]
-            for m in extensions:
+        try:
+            # Set the BL Wrapper to the bot so that it can be used during cog startup
+            self.bot.bl_wrapper = self.bl_wrapper
+
+            if module_name.lower() == "all":
+                extensions = [name for name in self.bot.extensions.keys()]
+                for m in extensions:
+                    try:
+                        self.bot.reload_extension(m)
+                        await ctx.send(f"Successfull reloaded {m.split('.')[-1]}")
+                    except Exception as e:
+                        await ctx.send(f"Failed to reload `{module_name}`")
+                        log.exception(f"Failed to reload {m.split('.')[-1]}")
+                    finally:
+                        log.warning(
+                            f"{ctx.author.display_name} reloaded {m.split('.')[-1]} from #{ctx.channel.name}"
+                        )
+            else:
+                module = f"UILayer.DiscordCommands.{module_name}"
                 try:
-                    self.bot.reload_extension(m)
-                    await ctx.send(f"""Successfull reloaded {m.split('.')[-1]}""")
+                    self.bot.reload_extension(module)
+                    await ctx.send(f"Successfully reloaded {module_name}")
+                except discord.ext.commands.errors.ExtensionNotLoaded:
+                    await ctx.send(
+                        f"Could not find a module matching the name `{module_name}`"
+                    )
                 except Exception as e:
-                    await ctx.send(f"""Failed to reload {m.split('.')[-1]}""")
-                    await handle_error(self.bot, e, traceback.format_exc())
-                print(
-                    f"{ctx.author.display_name} reloaded {m.split('.')[-1]} from #{ctx.channel.name}"
-                )
-        else:
-            module = f"UILayer.DiscordCommands.{module_name}"
-            try:
-                self.bot.reload_extension(module)
-                await ctx.send(f"Successfully reloaded {module_name}")
-                print(
-                    f"{ctx.author.display_name} reloaded {module_name} from #{ctx.channel.name}"
-                )
-            except discord.ext.commands.errors.ExtensionNotLoaded:
-                await ctx.send(
-                    f"Could not find a module matching the name `{module_name}`"
-                )
-            except Exception as e:
-                await ctx.send(f"Failed to reload `{module_name}`")
-                await handle_error(self.bot, e, traceback.format_exc())
+                    await ctx.send(f"Failed to reload `{module_name}`")
+                    log.exception(f"Failed to reload {module_name}")
+                finally:
+                    log.warning(
+                        f"{ctx.author.display_name} reloaded {module_name} from #{ctx.channel.name}"
+                    )
+        finally:
+            # Remove the BL from the bot to prevent it from being used in the wrong places
+            del self.bot.bl_wrapper
 
 
 def setup(bot):
