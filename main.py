89e665d458e976374f5ddbfe7e570efad086dc8d
Hrólfur <hrolfurgylfa@protonmail.com>
2021-11-27 23:29:10 +0000

    Added sleep time since the error was still occurring

--- main.py
+++ main.py
@@ -121,7 +121,7 @@ async def on_ready():
     # Start the SQL Manager
     print("Starting SQL Manager...")
     bot.sql = await SQLManager.start(bot, keys["SQL_Password"])
-    await asyncio.sleep(1)
+    await asyncio.sleep(2)
 
     # Start the officer Manager
     print("Starting Officer Manager...")
