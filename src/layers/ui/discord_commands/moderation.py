3cb0350f430e8fa1d5364a5f3a342933f78681fa
Hrólfur <hrolfurgylfa@protonmail.com>
2022-02-01 13:32:51 +0000

    Removed unused moderation commands

--- src/layers/ui/discord_commands/moderation.py
+++ src/layers/ui/discord_commands/moderation.py
@@ -20,30 +20,6 @@ class Moderation(commands.Cog):
         self.bl_wrapper: BusinessLayerWrapper = bot.bl_wrapper
         self.color = discord.Color.blue()
 
-    @checks.is_chat_moderator()
-    @commands.command()
-    async def detain(self, ctx):
-        """detain description"""
-        pass
-
-    @checks.is_moderator()
-    @commands.command()
-    async def restore(self, ctx):
-        """restore description"""
-        pass
-
-    @checks.is_chat_moderator()
-    @commands.command()
-    async def strike(self, ctx):
-        """strike description"""
-        pass
-
-    @checks.is_chat_moderator()
-    @commands.command()
-    async def list_strikes(self, ctx):
-        """list_strikes description"""
-        pass
-
 
 def setup(bot):
     bot.add_cog(Moderation(bot))
