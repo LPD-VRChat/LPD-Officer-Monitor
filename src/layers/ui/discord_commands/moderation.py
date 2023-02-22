21d16281c8bd0effc7f1a1f6a4bf0e28fc0b7568
50thomatoes50 <50thomatoes50@users.noreply.github.com>
2023-02-22 22:09:36 +0100

    mod discord command detain, release

--- src/layers/ui/discord_commands/moderation.py
+++ src/layers/ui/discord_commands/moderation.py
@@ -113,6 +113,103 @@ class Moderation(commands.Cog):
             interac, f"Stikes against user <@{user_id}> [{user_id}] :\n{message}"
         )
 
+    @checks.is_admin_bot_channel(True)
+    @checks.is_white_shirt(True)
+    @app_cmd.command(
+        name="detain_user",
+        description="Detain a user",
+    )
+    @app_cmd.guilds(discord.Object(id=settings.SERVER_ID))
+    @app_cmd.default_permissions(administrator=True)
+    @app_cmd.describe(offender="use this if user is still present in the guild")
+    @app_cmd.describe(member_id="use this if user is gone form the guild")
+    async def detain_user(
+        self,
+        interac: discord.Interaction,
+        reason: str,
+        offender: discord.Member = None,
+        member_id: str = None,
+    ):
+        """use `offender` if user is still present in the guild. use `member_id` if user is gone form the guild"""
+
+        match (offender is None, member_id is None):
+            case (True, True):
+                await interaction_reply(interac, f"Define only one user, {__doc__}")
+                return
+            case (False, False):
+                await interaction_reply(
+                    interac, f"You need to define a user!!!, {__doc__}"
+                )
+                return
+            case (False, True):
+                user_id = offender.id
+            case (True, False):
+                try:
+                    user_id = int(member_id)
+                except (ValueError):
+                    await interaction_reply(interac, f"Not a valid id, {__doc__}")
+                    return
+
+        if not await msgbox_confirm(
+            interac,
+            message=f"**Are you sure you want this user to be detained?**\nOffender: <@{user_id}  [{user_id}]\nReason: {reason}",
+        ):
+            return
+        await self.bl_wrapper.mod.detain_user(user_id, reason)
+        await interaction_reply(interac, f"User have been detained")
+
+    @checks.is_admin_bot_channel(True)
+    @checks.is_white_shirt(True)
+    @app_cmd.command(
+        name="release_detained_user",
+        description="Release detained user",
+    )
+    @app_cmd.guilds(discord.Object(id=settings.SERVER_ID))
+    @app_cmd.default_permissions(administrator=True)
+    @app_cmd.describe(offender="use this if user is still present in the guild")
+    @app_cmd.describe(member_id="use this if user is gone form the guild")
+    async def release_detained_user(
+        self,
+        interac: discord.Interaction,
+        offender: discord.Member = None,
+        member_id: str = None,
+    ):
+        """use `offender` if user is still present in the guild. use `member_id` if user is gone form the guild"""
+
+        match (offender is None, member_id is None):
+            case (True, True):
+                await interaction_reply(interac, f"Define only one user, {__doc__}")
+                return
+            case (False, False):
+                await interaction_reply(
+                    interac, f"You need to define a user!!!, {__doc__}"
+                )
+                return
+            case (False, True):
+                user_id = offender.id
+            case (True, False):
+                try:
+                    user_id = int(member_id)
+                except (ValueError):
+                    await interaction_reply(interac, f"Not a valid id, {__doc__}")
+                    return
+
+        if not await msgbox_confirm(
+            interac,
+            message=f"**Are you sure you want this user to be Released?**\nOffender: <@{user_id}  [{user_id}]",
+        ):
+            return
+
+        result = await self.bl_wrapper.mod.release_detained_user(user_id)
+
+        if result:
+            await interaction_reply(interac, f"User have been released")
+        else:
+            await interaction_reply(
+                interac,
+                f"One of the released operation may have failed.\nPlease check the bot logs",
+            )
+
 
 async def setup(bot):
     await bot.add_cog(Moderation(bot))
