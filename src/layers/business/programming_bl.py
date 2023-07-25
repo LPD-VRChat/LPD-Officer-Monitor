f69f87ae078c2446a9b05d07d7a5c7802129e803
50thomatoes50 <50thomatoes50@users.noreply.github.com>
2023-07-25 20:58:25 +0200

    listen for slash command fails and respond

--- src/layers/business/programming_bl.py
+++ src/layers/business/programming_bl.py
@@ -11,7 +11,7 @@ from discord.ext import commands
 
 # Custom
 import settings
-from src.layers.business.base_bl import DiscordListenerMixin
+from src.layers.business.base_bl import DiscordListenerMixin, bl_listen
 from src.layers.storage.models import database
 
 if TYPE_CHECKING:
@@ -24,6 +24,7 @@ log = logging.getLogger("lpd-officer-monitor")
 class ProgrammingBL(DiscordListenerMixin):
     def __init__(self, bot: commands.bot) -> None:
         self.bot = bot
+        super().__init__()
 
     async def clean_shutdown(
         self,
@@ -60,3 +61,17 @@ class ProgrammingBL(DiscordListenerMixin):
 
         if exit_code:
             _exit(exit_code)
+
+    @bl_listen("on_message")
+    async def detect_slash_cmd_fail(self, message: discord.Message):
+        if (
+            message.author.bot
+            or message.channel.id not in settings.ALLOWED_COMMAND_CHANNELS
+        ):
+            return
+        if message.content.startswith("/"):
+            await message.reply(
+                """:red_circle::red_circle:The command didn't work.:red_circle::red_circle:
+Make sure you turn off `Legacy chat input` in `Accessibility`.
+Make sure when you are typing the command that Discord is showing the command name back with a description"""
+            )
