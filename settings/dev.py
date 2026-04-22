239face2d0368e38383720041427aafc4c1c8c76
50thomatoes50 <50thomatoes50@users.noreply.github.com>
2026-04-22 22:34:09 +0200

    use pymysql driver instead of native because of issues with poetry

--- settings/dev.py
+++ settings/dev.py
@@ -130,7 +130,7 @@ DB_NAME = "LPD_Officer_Monitor"
 DB_USER = "lpd"
 DB_HOST = "localhost"
 DB_SOCK = "/run/mysqld/mysqld.sock"
-DB_TYPE = "mysql"
+DB_TYPE = "mysql+pymysql"
 DB_PORT = 3306
 
 LOG_PERMISSION_DENIED_ENABLE = True
